fe3a727a90b2e5e011720897dbe3730424ddf5e3
Eran Z <eran@zimbler.net>
2012-01-04 08:34:59 +0200

    Changing into a class and adding a hash for the commands

--- shituf.rb
+++ shituf.rb
@@ -1,71 +1,83 @@
 # encoding: UTF-8
 
 require 'gmail'
-
-@username = 'example@gmail.com'
-@password = 'secret'
-@registered =[]
-
-def loadRegistered(path)
-	File.open(path).each_line{ |line|
-	@registered << line
-	}
-	rescue
-	
-end
-def saveRegistered(path)
-	file = File.open(path,"w+")
-	@registered.each do |address|
-		file << address
+class Shituf
+	def initialize(username,password)
+	@username = username
+	@password = password
+	@registered =[]
 	end
-	file.close
-end
-def saveAttachments(attlist)
-	attlist.each do |a|
-		file = File.new(".\\attach\\" + a.filename, "w+")
-		file << a.decoded
+	attr_accessor :registered
+	Commands = {
+		"ADD ME" => ->(name){name.each do |n|; @registered << n if !@registered.include? n; end },
+		"REMOVE ME" => ->(name){ name.each do |n|; @registered.delete n;end },
+		"OTHER" => ->(n=0){puts "Do Nothing" }
+	}
+	def run_command(cmd,*param)
+		instance_exec(param, &Commands[cmd]) if Commands.key?(cmd)
+	end
+	def loadRegistered(path)
+		File.open(path).each_line{ |line|
+		@registered << line
+		}
+		rescue
+		
+	end
+	def saveRegistered(path)
+		file = File.open(path,"w+")
+		@registered.each do |address|
+			file << address
+		end
 		file.close
 	end
-end
-def foreward(msg)
-	if msg.attachments 
-		saveAttachments(msg.attachments)
-		#msg.save_attachments_to('./attach/')
-	end
-	@registered.each do |addr|
-		forwarded = Mail.new do
-			to addr
-			subject msg.subject
-			html_part do
-				content_type 'text/html; charset=UTF-8'
-				body msg.html_part.decoded
-			end
-			msg.attachments.each do |att|
-				add_file (".\\attach\\"+att.filename)
-			end
+	def saveAttachments(attlist)
+		attlist.each do |a|
+			file = File.new(".\\attach\\" + a.filename, "w+")
+			file << a.decoded
+			file.close
 		end
-		@gmail.deliver(forwarded)
 	end
-end
-def action(msg)
-	if msg.subject.upcase == "ADD ME"
-		@registered << msg.from.first unless @registered.include? msg.from.first
-	elsif msg.subject.upcase == "REMOVE ME"
-		@registered.delete(msg.from.first)
-	else
-		foreward(msg)
+	def foreward(msg)
+		if msg.attachments 
+			saveAttachments(msg.attachments)
+			#msg.save_attachments_to('./attach/')
+		end
+		forwarded = msg.clone
+		@registered.each do |addr|
+			#forwarded = Mail.new do
+			#	to addr
+			#	subject msg.subject
+			#	html_part do
+			#		content_type 'text/html; charset=UTF-8'
+			#		body msg.html_part.decoded
+			#	end
+			#	msg.attachments.each do |att|
+			#		add_file (".\\attach\\"+att.filename)
+			#	end
+			#end
+			forwarded.to = addr
+			@gmail.deliver(forwarded)
+		end
+	end
+	def action(msg)
+		if Commands.key? msg.subject.upcase
+			run_command(msg.subject.upcase,msg.from.first)
+		else
+			foreward(msg)
+		end
+	end
+
+	def shareMail
+		@gmail = Gmail.new(@username,@password)
+		loadRegistered("list.txt")
+		(@gmail.inbox.emails(:unread)).each do |msg|
+			puts msg.subject
+			action(msg)
+		end
+		saveRegistered("list.txt")
 	end
 end
 
-def shareMail
-	@gmail = Gmail.new(@username,@password)
-	loadRegistered("list.txt")
-	(@gmail.inbox.emails(:unread)).each do |msg|
-		puts msg.subject
-		action(msg)
-	end
-	saveRegistered("list.txt")
-end
+s = Shituf.new('username','password')
 
-
-shareMail()
+s.shareMail()
